28187433f0fbfcd3c8348710bf8a1dc370628168
dbwcooper <dbwencookie@gmail.com>
2022-02-12 13:41:59 +0800

    feat: use tailwindcss & nextjs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00b38a",
+        "primary-1": "#00a982",
+      },
+    },
+  },
+  plugins: [],
+};
